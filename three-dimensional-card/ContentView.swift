e6019b869b54696681d1682c3823829ba4cc6e7c
seonghun jeon <seonghun.jeon@illuminarean.com>
2024-08-08 15:10:56 +0900

    Initial Commit

---
+++ three-dimensional-card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  three-dimensional-card
+//
+//  Created by 전성훈 on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ three-dimensional-card/three_dimensional_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  three_dimensional_cardApp.swift
+//  three-dimensional-card
+//
+//  Created by 전성훈 on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct three_dimensional_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
